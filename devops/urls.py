8c6783da0e467579628041f774f7e4f8d7786fb4
zyndev <zyndev@gmail.com>
2017-10-10 19:54:27 +0800

    add micro

--- devops/urls.py
+++ devops/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^microservice/', include('microservice.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
